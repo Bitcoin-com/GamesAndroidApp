4f93456d15bf2e5a2ca7e21083752665ef7a853d
Joakim Johansson <joakim@safello.com>
2017-04-19 14:33:29 +0200

    count up time improvement

--- app/src/main/java/com/bitcoin/games/app/DiceActivity.java
+++ app/src/main/java/com/bitcoin/games/app/DiceActivity.java
@@ -1102,20 +1102,14 @@ public class DiceActivity extends GameActivity {
           }
 
           if (result.intwinnings > 0) {
-            long delta = mCreditBTCValue;
+            long delta;
             long quotient = result.intwinnings / mCreditBTCValue;
             if (quotient < 50) {
               delta = mCreditBTCValue;
             } else if (quotient < 500) {
               delta = mCreditBTCValue * 5;
-            } else if (quotient < 1000) {
-              delta = mCreditBTCValue * 50;
-            } else if (quotient < 2000) {
-              delta = mCreditBTCValue * 100;
-            } else if (quotient < 5000) {
-              delta = mCreditBTCValue * 200;
             } else {
-              delta = mCreditBTCValue * 300;
+              delta = quotient * 350;
             }
 
             if (mIsAutoOn) {
@@ -1137,7 +1131,6 @@ public class DiceActivity extends GameActivity {
         }
       });
       mShowLuckyNumberRunnable.run();
-
     }
 
     @Override
