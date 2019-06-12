e4bc5a45aa71af9c0eff39e07e61fcf137fe8a6e
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2019-06-12 17:22:25 +0900

    GAMES-841 - Implementing new design.

--- app/src/main/java/com/bitcoin/games/app/IntroActivity.java
+++ app/src/main/java/com/bitcoin/games/app/IntroActivity.java
@@ -5,14 +5,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.view.animation.AccelerateInterpolator;
-import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.Animation.AnimationListener;
-import android.view.animation.AnimationSet;
 import android.view.animation.AnimationUtils;
-import android.view.animation.DecelerateInterpolator;
-import android.widget.ImageView;
 
 import com.bitcoin.games.R;
 import com.bitcoin.games.lib.CommonActivity;
@@ -20,140 +15,66 @@ import com.bitcoin.games.settings.CurrencySettings;
 
 public class IntroActivity extends CommonActivity {
 
-  ImageView mTitleImage;
-  boolean mStartedNewIntent;
-
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    requestWindowFeature(Window.FEATURE_NO_TITLE);
-    getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-    setContentView(R.layout.activity_intro);
-    mTitleImage = findViewById(R.id.title_image);
-    mStartedNewIntent = false;
-  }
-
-  @Override
-  protected void onDestroy() {
-    super.onDestroy();
-  }
-
-  private void animate(final ImageView imageView, final int images[], final int imageIndex) {
-
-    //imageView <-- The View which displays the images
-    //images[] <-- Holds R references to the images to display
-    //imageIndex <-- index of the first image to show in images[]
-    //forever <-- If equals true then after the last image it starts all over again with the first image resulting in an infinite loop. You have been warned.
-
-    int fadeInDuration = 500; // Configure time values here
-    int timeBetween = 2000;
-    int fadeOutDuration = 500;
-
-    imageView.setVisibility(View.INVISIBLE);    //Visible or invisible by default - this will apply when the animation ends
-    imageView.setImageResource(images[imageIndex]);
-
-    Animation fadeIn = new AlphaAnimation(0, 1);
-    fadeIn.setInterpolator(new DecelerateInterpolator()); // add this
-    fadeIn.setDuration(fadeInDuration);
-
-    Animation fadeOut = new AlphaAnimation(1, 0);
-    fadeOut.setInterpolator(new AccelerateInterpolator()); // and this
-    fadeOut.setStartOffset(fadeInDuration + timeBetween);
-    fadeOut.setDuration(fadeOutDuration);
-
-    AnimationSet animation = new AnimationSet(false); // change to false
-    animation.addAnimation(fadeIn);
-    animation.addAnimation(fadeOut);
-    animation.setRepeatCount(1);
-    imageView.setAnimation(animation);
-
-    animation.setAnimationListener(new AnimationListener() {
-      public void onAnimationEnd(Animation animation) {
-          /*
-            if (images.length - 1 > imageIndex) {
-		            animate(imageView, images, imageIndex + 1,forever); //Calls itself until it gets to the end of the array
-				}
-				else {
-				    if (forever == true){
-					    animate(imageView, images, 0,forever);  //Calls itself to start the animation all over again in a loop if forever = true
-			        }
-			    }
-			    */
-      }
-
-      public void onAnimationRepeat(Animation animation) {
-        // TODO Auto-generated method stub
-      }
-
-      public void onAnimationStart(Animation animation) {
-        // TODO Auto-generated method stub
-      }
-    });
-  }
-
-//  public void onContainer(View button) {
-//    startActivity();
-//  }
-
-  private void startActivity() {
-    if (!mStartedNewIntent) {
-      startActivity(new Intent(this,
-        CurrencySettings.getInstance(this).getAccountKey() == null ? CreateAccountActivity.class : MainActivity.class));
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_intro);
     }
-  }
 
-  private void fadeIn() {
-    final View view = findViewById(R.id.container);
+    private void startActivity() {
+        startActivity(new Intent(this,
+            CurrencySettings.getInstance(this).getAccountKey() == null ? CreateAccountActivity.class : MainActivity.class));
+    }
 
-    Animation a = AnimationUtils.loadAnimation(this, android.R.anim.fade_in);
-    a.setDuration(500);
-    a.setAnimationListener(new AnimationListener() {
+    private void fadeIn() {
+        final View view = findViewById(R.id.container);
 
-      public void onAnimationEnd(Animation animation) {
-        view.setVisibility(View.VISIBLE);
-        mHandler.postDelayed(() -> fadeOut(), 1500);
-      }
+        Animation a = AnimationUtils.loadAnimation(this, android.R.anim.fade_in);
+        a.setDuration(500);
+        a.setAnimationListener(new AnimationListener() {
 
-      public void onAnimationRepeat(Animation animation) {
-      }
+            public void onAnimationEnd(Animation animation) {
+                view.setVisibility(View.VISIBLE);
+                mHandler.postDelayed(() -> fadeOut(), 1500);
+            }
 
-      public void onAnimationStart(Animation animation) {
-      }
+            public void onAnimationRepeat(Animation animation) {
+            }
 
-    });
-    view.startAnimation(a);
-  }
+            public void onAnimationStart(Animation animation) {
+            }
 
-  private void fadeOut() {
-    final View view = findViewById(R.id.container);
+        });
+        view.startAnimation(a);
+    }
 
-    Animation a = AnimationUtils.loadAnimation(this, android.R.anim.fade_out);
-    a.setDuration(500);
-    a.setAnimationListener(new AnimationListener() {
+    private void fadeOut() {
+        final View view = findViewById(R.id.container);
 
-      public void onAnimationEnd(Animation animation) {
-        view.setVisibility(View.INVISIBLE);
-        startActivity();
-      }
+        Animation a = AnimationUtils.loadAnimation(this, android.R.anim.fade_out);
+        a.setDuration(500);
+        a.setAnimationListener(new AnimationListener() {
 
-      public void onAnimationRepeat(Animation animation) {
-      }
+            public void onAnimationEnd(Animation animation) {
+                view.setVisibility(View.INVISIBLE);
+                startActivity();
+            }
 
-      public void onAnimationStart(Animation animation) {
-      }
+            public void onAnimationRepeat(Animation animation) {
+            }
 
-    });
-    view.startAnimation(a);
-  }
+            public void onAnimationStart(Animation animation) {
+            }
 
-  @Override
-  public void onResume() {
-    super.onResume();
-    mHandler.postDelayed(this::fadeIn, 1000);
-  }
+        });
+        view.startAnimation(a);
+    }
 
-  @Override
-  public void onPause() {
-    super.onPause();
-  }
+    @Override
+    public void onResume() {
+        super.onResume();
+        mHandler.postDelayed(this::fadeIn, 1000);
+    }
 }
--- app/src/main/java/com/bitcoin/games/app/SlotsHelpActivity.java
+++ app/src/main/java/com/bitcoin/games/app/SlotsHelpActivity.java
@@ -8,34 +8,19 @@ import android.view.WindowManager;
 
 import com.bitcoin.games.R;
 
-import de.marcreichelt.android.RealViewSwitcher;
-
 public class SlotsHelpActivity extends Activity {
 
-  RealViewSwitcher mViewSwitcher;
-  final static String TAG = "SlotsHelpActivity";
+    final static String TAG = "SlotsHelpActivity";
 
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    Log.v(TAG, "SlotsHelpActivity onCreate");
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.v(TAG, "SlotsHelpActivity onCreate");
 
-    requestWindowFeature(Window.FEATURE_NO_TITLE);
-    getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-    setContentView(R.layout.activity_slots_help);
-
-        /*
-        mViewSwitcher = new RealViewSwitcher(getApplicationContext());
-        int[] imageResources = new int[] { R.drawable.slt_help_howtoplay, R.drawable.slt_help_paytables1, R.drawable.slt_help_paytables2 };
-        for( int i = 0; i < imageResources.length; i++ ) {
-            ImageView view = new ImageView(getApplicationContext());
-            view.setImageResource( imageResources[i] );
-            mViewSwitcher.addView(view);
-        }
-        setContentView(mViewSwitcher);
-        */
-  }
-
+        setContentView(R.layout.activity_slots_help);
+    }
 }
 
